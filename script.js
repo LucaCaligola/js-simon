4a6cfcc446e7b34ee8367a2a1516a16d27023928
LucaCaligola <luca.caligola@gmail.com>
2023-11-26 12:58:18 +0100

    i created the countdown date and i found the difference between the final date and now

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la fine 
 // di quest'anno! Che scadra' quindi a capodanno!
+
+const giorniEl = document.querySelector('#giorni');
+const oreEl = document.querySelector('#ore');
+const minutiEl = document.querySelector('#minuti');
+const secondiEl = document.querySelector('#ore');
+
+// Mi memorizzo la data che voglio raggiungere
+let capodanno = new Date (2024,0,1,0,0,0,0);
+
+// trovo la data di adesso
+let adesso = new Date();
+
+// trovo la differenza con adesso
+const dateDifference = capodanno.getTime() - adesso.getTime()
+
+console.log(dateDifference)
+
+const secondi = Math.floor(dateDifference % (60  * 1000)) / 1000;
+const minuti = Math.floor(dateDifference % (60 * 60 * 1000)) / (1000 * 60);
+const ore = Math.floor(dateDifference % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60);
+const giorni = Math.floor(dateDifference / (24 * 60 * 60 * 1000));
+
+console.log(secondi)
+console.log(minuti)
+console.log(ore)
+console.log(giorni)
+
+// scrivo e aggiorno in pagina il countdown
